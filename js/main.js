24a5765d8cce4f0057fd2f1d2772729663ba773f
seadonggyun4 <seadonggyun@gmail.com>
2021-12-26 02:40:54 +0900

    💄 made a scroll animation for each section

--- js/main.js
+++ js/main.js
@@ -173,3 +173,25 @@ function floatingObject(selector, delay, size){
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+/* ============================== ALL: scroll position animation ============================== */
+const spyEls = document.querySelectorAll('section.scroll-spy');
+
+/*
+구조:
+
+ScrollMagic 에서 추가한 옵션들을 -> addTo(ScrollMagic.Contoller)를 통해내부의 컨트롤러에 내용을 할당해
+실제로 동작할수 있는 구조
+*/
+
+spyEls.forEach(function(spyEl){
+  //Scene: 특정 요소를 감지하는 메소드
+  //setClassToggle: class 를 토글 시켜주는 메소드
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 //감시할 요소를 인지, 식별하는 지점을 명시 -> 뷰포트 최상단 0 ~ 뷰포트 최하단 1 로 환산한다. 
+    })
+    .setClassToggle(spyEl, 'show') // setClassToggle(적용될 식별자, 토글될 클래스 명)
+    .addTo(new ScrollMagic.Controller());
+})
