4e0c458225b4ff093fa16f080790dcbcfd7b5b67
seadonggyun4 <seadonggyun@gmail.com>
2021-12-25 18:36:12 +0900

    💄 made a floating animation

--- js/main.js
+++ js/main.js
@@ -144,5 +144,32 @@ promotionToggleBtn.addEventListener('click', function(){
   }
 });
 
+/* ============================== YOUTUBE VIEDO: floating animation ============================== */
+// [범위 랜덤 함수(소수점 2자리까지)]
+// random(최솟값, 최댓값): 최솟값 ~ 최댓값 사이의 랜덤한 값을 적용한다.
+function random(min, max) { 
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
 
 
+function floatingObject(selector, delay, size){
+  // gsap.to(요소,시간,적용스타일)
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+      y: size, // y축으로 얼마나 움직일지
+      repeat: -1, // 반복 여부: -1은 무한반복을 의미
+      yoyo: true, // yoyo 는 한번재생된 애니메이션을 역으로 다시 재생할수있는 함수
+      ease: Power1.easeInOut, //easing 함수
+      delay: random(0, delay) 
+    }
+  )
+}
+
+//css 선택자를 넣을수 있다.
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
